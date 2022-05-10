fccf3906b6e2f2022c4665d3b95dd81891ae2c2f
youssefmagdyy <youssefmagdy1910@gmail.com>
2022-05-10 16:28:07 +0200

    envs

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.get('/',(req,res)=>{res.send('API')})
 app.use('/exercises',exercisesRoutes)
 
-const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb+srv://youssefmagdyy:094HP9wmjRH6nX7i@cluster0.sitlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL).then(()=>app.listen(PORT,()=>{console.log(`Server On ${PORT}`)})).catch((error)=>{console.log(error.message)});
